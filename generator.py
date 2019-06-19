cbd3cbec6e4ac6ae724bdbc90b419f66a4cf5894
Eugenio Antonio Bonanno <eugenioantoniobonanno@gmail.com>
2019-06-20 02:17:48 +0800

    Lint generator.py

--- generator.py
+++ generator.py
@@ -1,12 +1,14 @@
-import openpyxl
 import os
 import pickle
-from dateutil.rrule import *
-from dateutil.parser import *
-from schedule_functions import find_month_length, format_sheet, write_schedule, get_days_missed, get_classes_subbed, get_monthly_meeting
-from user_functions import register_user, login_user, create_schedule, remove_class, add_class, view_schedule
-from pathlib import Path
 import logging
+from pathlib import Path
+from dateutil.rrule import rrule, DAILY
+from dateutil.parser import parse
+import openpyxl
+from schedule_functions import find_month_length, format_sheet, write_schedule, get_days_missed, get_classes_subbed, \
+    get_monthly_meeting
+from user_functions import register_user, login_user, create_schedule, remove_class, add_class, view_schedule
+
 
 root = Path(".")
 
@@ -33,7 +35,7 @@ def make_skipped_days_filter(skipped_days):
 
 while True:
     user_choice = input("Hello, please enter 'login' to login, or type 'register' to create an account: \n ")
-    logger.info('A user entered: ' + user_choice)
+    logger.info("A user entered: " + user_choice)
 
     if user_choice.lower() == 'register':
         register_user()
@@ -44,32 +46,33 @@ while True:
 
         while True:
             make_or_write = input("Enter 'set' create a new schedule, 'add' to add classes to your current one, "
-                                  "'remove' to remove a class, 'view' to see your current schedule, or 'export' to create a copy of it: \n")
-            logger.info(active_user + ' has chosen the option: ' + make_or_write)
+                                  "'remove' to remove a class, 'view' to see your current schedule, or 'export' to "
+                                  "create a copy of it: \n")
+            logger.info(active_user + " has chosen the option: " + make_or_write)
             if make_or_write.lower() == "set":
                 create_schedule(active_user, make_or_write)
-            elif make_or_write.lower() == 'add':
+            elif make_or_write.lower() == "add":
                 add_class(active_user)
-            elif make_or_write.lower() == 'view':
+            elif make_or_write.lower() == "view":
                 view_schedule(active_user)
             elif make_or_write.lower() == "export":
                 users_object_path = root / "user_objects" / active_user
-                schedule = open(users_object_path, 'rb')
+                schedule = open(users_object_path, "rb")
                 users_schedule = pickle.load(schedule)
                 break
 
-            elif make_or_write.lower() == 'remove':
+            elif make_or_write.lower() == "remove":
                 remove_class(active_user)
 
             else:
                 logger.debug("Sorry that wasn't one of the options, please try again.")
-                logger.info(active_user + ' could not proceed with option '+ make_or_write + ' due to invalid input.')
-
+                logger.info(active_user + " could not proceed with option  "+ make_or_write + " due to invalid input. ")
 
         year = "2019"
 
         while True:
-            possible_months = ['01', '1', '2', '02', "3", '03', "4", '04', "5", '05', "6", '06', "7", '07', "8", '08', "9", '09', "10", '11', '12']
+            possible_months = ['01', '1', '2', '02', "3", '03', "4", '04', "5", '05', "6", '06', "7", '07', "8", '08',
+                               "9", '09', "10", '11', '12']
             month = input("Please input a month as a numeric value [EX 4 for April]: \n")
             logger.info(active_user, 'set the month to', month, 'well making schedule.')
             if len(month) == 1:
@@ -78,7 +81,8 @@ while True:
                 break
             else:
                 logger.debug("Sorry I can't make sense of what month you mean. Please try again.")
-                logger.info(active_user + ' set the month to something that is not recognized as a month: ' + month + ' well making schedule.')
+                logger.info(active_user + ' set the month to something that is not recognized as a month:'
+                                          ' ' + month + ' well making schedule.')
 
         # Generates a list of user input representing days they missed work
         days_to_skip = get_days_missed(active_user)
@@ -87,7 +91,8 @@ while True:
         end = find_month_length(month, year)
 
         # Creates a list of all the days in the month
-        date_range = list(rrule(DAILY, dtstart=parse("2019" + month + "01T090000"), until=parse("2019" + month + end + "T090000")))
+        date_range = list(rrule(DAILY, dtstart=parse("2019" + month + "01T090000"),
+                                until=parse("2019" + month + end + "T090000")))
         days_to_schedule = list(filter(make_skipped_days_filter(days_to_skip), date_range))
 
         monthly_meeting = get_monthly_meeting(active_user)
@@ -106,13 +111,15 @@ while True:
             if not os.path.isdir("paysheets"):
                 os.makedirs("paysheets")
             workbook.save(os.path.join('paysheets', active_user + "paysheet" + '.xlsx'))
-            logger.debug("Your Paysheet has been created and saved and should be available in a folder name 'paysheets' located inside the folder containing this program.")
+            logger.debug("Your Paysheet has been created and saved and should be available in a folder name 'paysheets'"
+                         " located inside the folder containing this program.")
             logger.info(active_user + ' successfully generated a paysheet.')
 
             break
         except:
-            logger.debug("An error occurred when attempting to save your Paysheet. Make sure no spreadsheets are currently open. "
-                  "If they are close them, and then retry well paying careful attention to the on screen instructions.")
+            logger.debug("An error occurred when attempting to save your Paysheet. Make sure no spreadsheets are "
+                         "currently open. If they are close them, and then retry well paying careful attention to "
+                         "the on screen instructions.")
             logger.info(active_user + ' encountered an error well generating a paysheet.')
             exit()
 
