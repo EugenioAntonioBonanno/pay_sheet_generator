f3db0b55e4001457df1d88cd903a684aac79f6b2
Manuel Christlieb <manuel@christlieb.eu>
2019-08-19 21:07:52 +0200

    use absolute paths in config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-user_db_path = os.path.join('data', 'users.db')
+user_db_path = os.path.abspath(os.path.join('data', 'users.db'))
 
-user_schedules_dir = os.path.join('data', 'schedules')
+user_schedules_dir = os.path.abspath(os.path.join('data', 'schedules'))
 
-pay_sheet_export_dir = os.path.join('data', 'export')
+pay_sheet_export_dir = os.path.abspath(os.path.join('data', 'export'))
